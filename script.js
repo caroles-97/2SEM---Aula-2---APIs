e0b2ba1a5055a7f32d7a8523b1abc209fe1df428
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-11 20:51:47 -0300

    Aula de API

--- script.js
+++ script.js
@@ -4,9 +4,40 @@
 // =============================================================
 
 const URL_PRODUTOS = "https://fakestoreapi.com/products";
+let todosOsProdutos = []
 
-const buscarProdutos = () => {}
+const buscarProdutos = async () => {
+    const resposta = await fetch(URL_PRODUTOS) //buscar
+    console.log(resposta)
+    //--- todo método assíncrono, retorna com uma promessa
 
+    const dados = await resposta.json() //pega a (resposta) e transforma em json
+    console.log(dados)
+
+    todosOsProdutos = dados
+    console.log(todosOsProdutos)
+    renderizarCard(todosOsProdutos)
+    console.log(todosOsProdutos)
+
+}
+
+//criar função renderizarCard
+const renderizarCard = listaProdutos => {
+    const grid = document.querySelector("#grid-categoria")
+    grid.innerHTML = ""  
+
+    listaProdutos.forEach(produto => {
+        const card = document.createElement("article");
+        card.className = "card";
+        card.innerHTML = `
+        <img src="${produto.image}" alt="${produto.title}" />
+        <h3>${produto.title}</h3>
+        <p class="preco">R$ ${produto.price.toFixed(2)}</p>
+        <p class="avaliacao">⭐ ${produto.rating.rate} (${produto.rating.count} avaliações)</p>`
+
+        grid.append(card)
+    })
+}
 
 // -------------------------------------------------------------
 // Disparo inicial — não mexer, só chama a função de fetch.
