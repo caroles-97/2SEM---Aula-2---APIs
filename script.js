293ac7cf1e6d8e24772b9547628701293ff98b93
Caio Oliveira <caio.oliveira07@gmail.com>
2026-08-11 18:24:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// =============================================================
+// AULA: Consumindo API + filter/map no DOM
+// API usada: https://fakestoreapi.com  (não precisa de token)
+// =============================================================
+
+const URL_PRODUTOS = "https://fakestoreapi.com/products";
+
+const buscarProdutos = () => {}
+
+
+// -------------------------------------------------------------
+// Disparo inicial — não mexer, só chama a função de fetch.
+// -------------------------------------------------------------
+buscarProdutos();
